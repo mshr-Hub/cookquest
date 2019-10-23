e971e467a64ce09328053139f61a5def85a42c41
mshr-Hub <masa.megane.616@gmail.com>
2019-10-24 03:12:58 +0900

    デプロイ修正

--- CookQuest/settings.py
+++ CookQuest/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
