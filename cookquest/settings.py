636f54e2ab692da80c30150a4edca1698c52a4a7
mshr-Hub <masa.megane.616@gmail.com>
2019-10-28 23:12:29 +0900

    S3修正設定

--- cookquest/settings.py
+++ cookquest/settings.py
@@ -153,8 +153,11 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+    AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+    }
+    DEFAULT_FILE_STORAGE = 'cookquest.backends.MediaStorage'
 
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
